d0e71b9b5ee151021c3d5429e1cda9ca9814378f
Bhaskar Trivedi <thimanshu003@gmail.com>
2024-02-19 13:16:46 +0530

    line added for testing

--- spring_boot_backend_template/src/main/java/com/app/entities/Customer.java
+++ spring_boot_backend_template/src/main/java/com/app/entities/Customer.java
@@ -67,3 +67,6 @@ public class Customer extends BaseEntity{
 //to be applied on password in dto
 //@Column(length = 20,nullable = false)
 //@Pattern(regexp="((?=.*\\d)(?=.*[a-z])(?=.*[#@$*]).{5,20})")
+
+
+// this is the line from bpojo1
